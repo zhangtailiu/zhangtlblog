7e6d7480ec461693058b7ea21f1a9bd06252b965
zhangtailiu <1021611629@qq.com>
2019-09-05 11:12:56 +0800

    mybatis配置

--- mgrsite/src/main/java/cn/zhangtl/zhangtlblog/ZhangtlblogApplication.java
+++ mgrsite/src/main/java/cn/zhangtl/zhangtlblog/ZhangtlblogApplication.java
@@ -1,9 +1,13 @@
 package cn.zhangtl.zhangtlblog;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@MapperScan
 public class ZhangtlblogApplication {
 
 	public static void main(String[] args) {
