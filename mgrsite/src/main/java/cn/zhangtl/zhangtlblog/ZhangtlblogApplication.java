f207678327d589a5a2ffda903d0466a8d872884c
zhangtailiu <1021611629@qq.com>
2019-09-05 09:16:36 +0800

    基本模块

---
+++ mgrsite/src/main/java/cn/zhangtl/zhangtlblog/ZhangtlblogApplication.java
@@ -0,0 +1,13 @@
+package cn.zhangtl.zhangtlblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhangtlblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhangtlblogApplication.class, args);
+	}
+
+}
