cd10a74931b758d5aa999b8c39681ee615f2fc95
zhangtailiu <1021611629@qq.com>
2019-10-22 16:49:07 +0800

    合并代码

--- mgrsite/src/test/java/cn/zhangtl/zhangtlblog/EasyExcelTest.java
+++ mgrsite/src/test/java/cn/zhangtl/zhangtlblog/EasyExcelTest.java
@@ -8,15 +8,29 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 public class EasyExcelTest {
 
     @Test
-    public void importTest(){
-        File file = new File("E:\\test.xlsx");
+    public void importTest() throws IOException {
+        File file = new File("E:\\大件预算.xlsx");
         List<Object> list = EasyExcel.read(file).autoCloseStream(true).excelType(ExcelTypeEnum.XLSX).sheet(0).doReadSync();
-        System.out.println(list);
+        StringBuffer sb = new StringBuffer();
+        for (Object o : list) {
+            sb.append("UPDATE EHREOS.T_EXPRESS_STAFFING STA SET ");
+            Map<Integer,String> dj = (Map) o;
+            sb.append("STA.RMS_DJNUM = ").append(dj.get(1)).append(" WHERE STA.DEPTNAME4 = \"").append(dj.get(3)).append("\";");
+        }
+        File file1 = new File("E:\\大件预算sql.sql");
+        FileOutputStream out = new FileOutputStream(file1);
+        out.write(sb.toString().getBytes());
+        out.flush();
+        out.close();
     }
 }
